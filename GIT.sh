06ba1742ffb482eed355f15f588c5a786853f0fc
Garali Med Karim <garalimedkarim@gmail.com>
2018-01-15 16:04:56 +0100

    Formation Node Js End

--- GIT.sh
+++ GIT.sh
@@ -6,3 +6,6 @@ add remote
 	git remote add pb https://github.com/paulboone/ticgit
 update remote url
 	git remote set-url origin https://github.com/USERNAME/REPOSITORY.git
+
+Clean files and directories:
+	git clean -df
